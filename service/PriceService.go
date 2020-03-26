ec1ab6edbe52f835d9ddfd2ae1d295d45732bab8
Waclaw.Dziewulski <Waclaw.Dziewulski@dynatrace.com>
2020-03-26 22:22:04 +0100

    WebScraper was split into methods, which perform the actual web page scraping and open graph extraction.

--- service/PriceService.go
+++ service/PriceService.go
@@ -48,14 +48,14 @@ func (service *PriceService) addPrice(priceData *model.PriceData) bool {
 // AddProductPriceByURL adds product to Database and returns the priceData result
 func (service *PriceService) AddProductPriceByURL(url string) *model.PriceData {
 
-	// 1. Check if the product url is in the Database
+	// (/) 1. Check if the product url is in the Database
 	// 2. If we have it, check the time of last update and see if makes sense to query the web page again
 	// 3a. If we have just get the data from the database and present it to the user
 	// 3b. If we haven't updated for a while, download the price data
 	// 4b. If we don't have an image yet, use the ImageURL to download the image
 	// 5. Attach the image data upon adding the entry to the database
 	// 6. Update the price of the product if we already have a product entry
-	priceData := <-web.ScrapePage(url)
+	priceData := <-web.ExtractPriceDataFromURL(url)
 	priceData.ImageData = <-web.DownloadImage(priceData.ImageURL)
 	priceData.AffiliateLink = <-service.urlShorteningService.ShortenURL(priceData.URL)
 
